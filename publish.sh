5e7ff924255692eeb2a957f457ac79b26834c993
Paul Mullins <personinternet@protonmail.com>
2023-06-07 02:20:38 -0400

    Added publish script

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export build_path='dentropy.github.io'
+node process_markdown.js
+cd $build_path
+git add .
+git commit -m "Updated Site"
+mkdocs build -v
+git add .
+git commit -m "Rebuilt site"
+git push origin main
