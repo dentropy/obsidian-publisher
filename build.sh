bed57296784918ed68755ac2e6d3fe5ecab06a3c
dentropy <dentropy@protonmail.com>
2023-10-19 16:47:54 -0400

    Updated default settings

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export build_path='dentropy.github.io'
 source .env
-node process_markdown.js -i '/home/paul/Documents/Root' -o './dentropy.github.io' -oi 5
+node process_markdown.js -i '/home/dentropy/Documents/Root' -o './dentropy.github.io' -oi 5
 # cd $build_path
 # git add .
 # git commit -m "Updated Site"
